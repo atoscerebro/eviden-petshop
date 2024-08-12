f18bdbb55658414eb34aef464a4e64c14fcff8e3
Giordano Corradini <giordano.corradini@globalpay.com>
2024-08-12 08:43:39 +0200

    fixed containerapp name

--- infra/containerapp.tf
+++ infra/containerapp.tf
@@ -1,5 +1,5 @@
 # resource "azurerm_container_registry" "container_registry" {
-#   name                = "examplecontainerregistry"
+#   name                = "${var.name}-cr"
 #   resource_group_name = azurerm_resource_group.petshop_rg.name
 #   location            = var.location
 #   sku                 = "Basic"
